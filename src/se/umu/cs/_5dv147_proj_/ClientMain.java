82c938df4f1d9aed3251670417d131bcb9f08251
Mikael Johansson <c10mjn@cs.umu.se>
2015-09-20 00:06:18 +0200

    First commit. Initialized the main Debug Settings Basic structure

---
+++ src/se/umu/cs/_5dv147_proj_/ClientMain.java
@@ -0,0 +1,21 @@
+package se.umu.cs._5dv147_proj_;
+
+/**
+ * Created by c10mjn on 19/09/15.
+ */
+public class ClientMain {
+
+    public static void main (String[] args) {
+
+        // Build Settings
+
+
+        // Build logic
+        // Logic has connections
+
+        // Build GUI
+
+
+
+    }
+}
